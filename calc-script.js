f59d8cff4234606b3d225318133c0f72a96121c2
dexter-idc <140709325+dexter-idc@users.noreply.github.com>
2023-09-12 17:25:13 +0530

    fixed the errors - repetition of operators, repetition of decimal points.

--- calc-script.js
+++ calc-script.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-style */
 const one = document.getElementById('btnOne');
 const two = document.getElementById('btnTwo');
 const three = document.getElementById('btnThree');
@@ -18,77 +19,121 @@ const div = document.getElementById('divBtn');
 const eqlBtns = document.getElementById('eqlBtn');
 const clear = document.getElementById('clrBtn');
 
-let result = 0;
-let newnumber = 0;
-let x = false;
-let currentInput = 0;
-let previousInput = 0;
-let srt = 0;
+let operator = 0;
 
+let result ;
 
-one.addEventListener('click', () => passNumber('1'));
-two.addEventListener('click', () => passNumber('2'));
-three.addEventListener('click', () => passNumber('3'));
+let temp;
 
-four.addEventListener('click', () => passNumber('4'));
-five.addEventListener('click', () => passNumber('5'));
-six.addEventListener('click', () => passNumber('6'));
+let inputNumber = 0;
 
-seven.addEventListener('click', () => passNumber('7'));
-eight.addEventListener('click', () => passNumber('8'));
-nine.addEventListener('click', () => passNumber('9'));
+let expression = 0;
 
-zero.addEventListener('click', () => passNumber('0'));
-decimalVal.addEventListener('click', () => displayNumber('.'));
+let foundOperator = false;
 
+let pressedEqual = false;
 
-addBtn.addEventListener('click', () => passOperator('+'));
-subBtn.addEventListener('click', () => passOperator('-'));
-mulBtn.addEventListener('click', () => passOperator('*'));
-divBtn.addEventListener('click', () => passOperator('/'));
+let count = 0;
 
+let lastChar;
 
+one.addEventListener('click', () => getNumber('1'));
+two.addEventListener('click', () => getNumber('2'));
+three.addEventListener('click', () => getNumber('3'));
+
+four.addEventListener('click', () => getNumber('4'));
+five.addEventListener('click', () => getNumber('5'));
+six.addEventListener('click', () => getNumber('6'));
+
+seven.addEventListener('click', () => getNumber('7'));
+eight.addEventListener('click', () => getNumber('8'));
+nine.addEventListener('click', () => getNumber('9'));
+
+zero.addEventListener('click', () => getNumber('0'));
+decimalVal.addEventListener('click', () => getNumber('.'));
+
+addBtn.addEventListener('click', () => getOperator('+'));
+subBtn.addEventListener('click', () => getOperator('-'));
+mulBtn.addEventListener('click', () => getOperator('*'));
+divBtn.addEventListener('click', () => getOperator('/'));
 
 clear.addEventListener('click', () => {
-    document.getElementById("numberDisplay").innerHTML = '';
-    document.getElementById('operatorDisplay').innerHTML = '';
-    srt=0;
+  document.getElementById('numberDisplay').innerHTML = '';
+  document.getElementById('operatorDisplay').innerHTML = '';
+  expression = 0;
+  result = 0;
 });
 
+eqlBtns.addEventListener('click', () => {
+  document.getElementById('operatorDisplay').innerHTML = '=';
+  result = math.evaluate(expression);
+  document.getElementById('numberDisplay').innerHTML = result;
+  console.log(result);
+  operator = 0;
+  count = 0;
+  expression = 0;
+  expression += result;
+  result = 0;
+  pressedEqual = true;
+  return result;
+});
 
-function displayNumber(numVal) {
-    document.getElementById("numberDisplay").innerHTML += numVal;
-    if (x == true) nextNum();
+function getOperator(oprVal) {
+  foundOperator = true;
+
+  if (!pressedEqual) {
+    lastChar = expression.charAt(expression.length - 1);
+    pressedEqual = true;
+    if (lastChar === '.') {
+      document.getElementById('numberDisplay').innerHTML = expression.charAt(expression.length - 2);
+      expression.slice(expression.length - 1);
+    }
+  }
+
+  document.getElementById('operatorDisplay').innerHTML = oprVal;
+  if (operator != oprVal) {
+    operator = oprVal;
+    expression += operator;
+    count = 0;
+  }
 }
 
-function passOperator(oprVal) {
-    x = true;
-    document.getElementById('operatorDisplay').innerHTML = oprVal;
-    console.log(srt)
-    srt += oprVal;
-    return x;
+function getNumber(num) {
+  if (num === '.') {
+    count++;
+  }
+
+  if (num === '.' && inputNumber.includes('.') || count > 1) {
+    console.log('decimal count overload');
+    count = 1;
+    return;
+  }
+
+  inputNumber = num;
+  console.log(inputNumber);
+  displayNumber(inputNumber);
+  expression += inputNumber;
+  console.log(expression);
+  operator = 0;
 }
 
-function passNumber(feed) {
-    newnumber = feed;
-    displayNumber(newnumber);
-    srt += newnumber;
-    return srt;
+function displayNumber(numberValue) {
+  clr();
+  document.getElementById('numberDisplay').innerHTML += numberValue;
+  if (foundOperator == true) nextNum();
 }
 
 function nextNum() {
-    document.getElementById("numberDisplay").textContent = newnumber;
-    document.getElementById('operatorDisplay').innerHTML = '';
-    x = false;
-    return x;
+  document.getElementById('numberDisplay').textContent = inputNumber;
+  document.getElementById('operatorDisplay').innerHTML = '';
+  foundOperator = false;
 }
 
-eqlBtns.addEventListener('click', () => {
-
-    document.getElementById('operatorDisplay').innerHTML = '=';
-    result = math.evaluate(srt);
-    console.log(result)
-    document.getElementById("numberDisplay").innerHTML=result;
-    srt=0
-    
-});
+function clr() {
+  if (pressedEqual == true) {
+    document.getElementById('numberDisplay').textContent = '';
+    document.getElementById('operatorDisplay').innerHTML = '';
+  }
+  pressedEqual = false;
+  // eslint-disable-next-line eol-last
+}
